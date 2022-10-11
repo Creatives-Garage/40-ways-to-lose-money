80c5e6c127df1b167d21b27bd5c9d2ad4ff888a1
zidanegimiga <zidanegimiga@gmail.com>
2022-10-11 12:45:32 +0300

    feat: add data source and image assets

---
+++ src/dataSource.ts
@@ -0,0 +1,57 @@
+export const data = [
+    {
+        name: "Ponzi",
+        image: "/Ponzi.png",
+        header: "Ponzi SCheme",
+        body: "So your friends made money from DECI and other Ponzi schemes, and despite your better judgement you decided not to send that 100K to mathe just yet. I mean, a 200% return on investment in two months? Who wouldn’t jump at the opportunity!? Boom! You put in your savings, Bang!, Their offices are closed! F*^% ! The money disappears as fast as you thought you would triple it."
+    },
+    {
+        name: "Pastor",
+        image: "/Pastor.png",
+        header: "Pastor",
+        body: "Your pastor and his wife have his and hers range rover. You still haggle in the matatu about the fare to church. He takes your money; you hold on to faith. Everyday in church there is a fundraiser to buy your spiritual leader things too good for mere mortals such as yourself to experience. Anyway, Hope is currency and your weekly keshas are investment accounts eh?"
+    },
+    {
+        name: "Luthuli",
+        image: "/Luthuli.png",
+        header: "Luthuli Avenue",
+        body: "When you go to Luthuli Avenue to buy a TV you will come out with a certificate from the ”Society Of Goods Reappropriation Professionals” . Walk down the street with that fancy new laptop, still in the box and these wonderful chaps will help relieve you of your burden, and maybe your wallet too. Don’t worry though, they often give back just enough money for you to get home."
+    },
+    {
+        name: "WashWash",
+        image: "/WashWash.png",
+        header: "Wash Wash",
+        body: "Turn money into more money. A man who is not even your pastor or saviour reborn (again) will miraculously turn funny coloured paper into money! He’s got a fancy suit and more bling than Atwoli and Mike Sonko at a Dubai jewellery expo.. You give him legit money. He buys special chemicals and dips the funny coloured paper in it. Suddenly your money has doubled! Now you completely believe him! He needs more money for more chemicals to make more fake money! Then…then he stops taking your calls."
+    },
+    {
+        name: "Nywele",
+        image: "/shuka.png",
+        header: "AKI BEB NITUMIE YA NYWELE",
+        body: "Her hair is always on fleek, mainly because her blesser (read: You) is always taking care of her hair. However, hair maintenance doesn’t cost a fraction of what you send her. You’re just an ATM with a receding hairline."
+    },
+    {
+        name: "Ben10",
+        image: "/ben10.png",
+        header: "BEN 10 WA AUNTY WA HARRIER",
+        body: "He has no job yet, everything is always JUST about to work out. Until then, however, he drives his girlfriend’s harrier around the city to hit up his friends and hit on her girlfriends."
+    },
+    {
+        name: "IPhoneXL",
+        image: "/HAND.png",
+        header: "IPHONE XL (SHHH! KWA BEI YA CHINI)",
+        body: "Nothing sounds as good as getting premium electronics on the cheap. Sure, the IPhone usually costs about KSh 50 000 more but this guy, in a back alley seems trustworthy? Right? Well, soon as he’s left with your money in hand you’ll find out that all you’ve got is a phone casing filled with mud, a double AA battery and a few lights."
+    },
+    {
+        name: "Shitstorm",
+        image: "/streetboy.png",
+        header: "SHITSTORM MEETS WHITE PANTS",
+        body: "They patrol Nairobi’s street like a roving pack of predators. Looking for unsuspecting victims, their weapon in hand. On particularly hot days, it becomes more potent. They spot you one day, dressed in white, on your way to meet your date. Three surround you and open their hands. Inside each palm is faecal matter, human or dog, you can’t tell. But the street kids’ demands aren’t unclear. It’s a shitty shakedown for shillings. You empty your pockets into an empty palm and they slip away. I guess the date is cancelled now, ay?"
+    },
+    {
+        name: "FakeLogBook",
+        image: "/LOGBOOK.png",
+        header: "FAKE LOGBOOK",
+        body: "You’ve saved up for years. Butter is a luxury you only see on the television. However, it was worth it, finally you’ve bought the car you’ve always wanted. What’s more, the guy even gave you a discount if you paid in cash! However, before you can even replace that new car smell with your own brand of funk, a car pulls up in front of you on the highway and three burly men pop out and rush towards you. Your car is up for auction, some clever monkey has cloned your log book and used it as security to take out loans from every single digital loan app, SACCO and dodgy logbook loan office on river road ."
+    },
+];
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,20 @@
-import React from "react"
+import React from "react";
+import styles from "../styles/index.module.scss"
+import Header from "components/Header";
+import Section from "components/Section";
+import BodyText from "components/Body";
+import { data } from "dataSource";
 
 export default function Index() {
   return (
-    <div>
-    </div>
+    <>
+      <Section bgColor="#">
+        <div className={styles.ponzi}></div>
+        <div className={styles.body}>
+          <Header level={2}>{data[0].header}</Header>
+          <BodyText>{data[0].body}</BodyText>
+        </div>
+      </Section>
+    </>    
   );
 }
