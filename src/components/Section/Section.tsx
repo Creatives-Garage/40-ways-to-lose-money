45d1583384bc2d800c12f113039be556a29e67af
zidanegimiga <zidanegimigatemp@gmail.com>
2023-11-14 10:00:16 +0300

    remove audio controls

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -43,7 +43,7 @@ const Section = ({ children, audio, bgColor }: SectionProps) => {
       className={styles.section} 
       style={{backgroundColor: bgColor}}
     >
-      <audio ref={audioRef} preload="metadata" controls loop>
+      <audio ref={audioRef} preload="metadata" loop>
         <source type="audio/mpeg" src={"/sounds/test.mp3"} />
       </audio>
       {children}
